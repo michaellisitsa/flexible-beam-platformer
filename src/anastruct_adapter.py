e615bc511b680da9f2db6102cf6ece455c4ce8b3
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro.local>
2023-02-07 09:34:06 +1100

    Add outline for FlexiblePlatform sprite

---
+++ src/anastruct_adapter.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+class FlexiblePlatform(pygame.sprite.Sprite):
+    def __init__(self):
+        # Define a flexible platform.
+        # Property initial platform position.
+        # Property initial support positions.
+        # Property section properties.
+        # Doesn't need to initialize anastruct just yet.
+
+        # def get_vertices(weight, position):
+        """
+        params: The weight of hero who is located on the platform.
+        params: The position of the hero along the beam
+        """
+
+    # def get_deflection_position():
+    # Called when their is a collision with the hero.
+    # Initialize anastruct and geometry based on platform position
+    # Switches out the platform for the deflected shape.
+
+    #
